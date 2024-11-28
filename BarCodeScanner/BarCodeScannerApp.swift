0ee12ed1b752078904e23f7492c9960de47d3bad
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-11-28 13:00:08 +0100

    Created blank project

---
+++ BarCodeScanner/BarCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerApp.swift
+//  BarCodeScanner
+//
+//  Created by Javier Calatrava on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
